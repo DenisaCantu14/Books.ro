68f52da48950389421e1b3c484232d6f77c26719
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-01-03 13:42:20 +0200

    dropdown

--- OnlineShop/Models/ShoppingCart.cs
+++ OnlineShop/Models/ShoppingCart.cs
@@ -147,40 +147,6 @@ namespace OnlineShop.Models
             // Return the OrderId as the confirmation number
             return order.OrderId;
         }
-        // We're using HttpContextBase to allow access to cookies.
-       /* public string GetCartId(HttpContextBase context)
-        {
-            if (context.Session[CartSessionKey] == null)
-            {
-                if (!string.IsNullOrWhiteSpace(context.User.Identity.Name))
-                {
-                    context.Session[CartSessionKey] =
-                        context.User.Identity.Name;
-                }
-                else
-                {
-                    // Generate a new random GUID using System.Guid class
-                    Guid tempCartId = Guid.NewGuid();
-                    // Send tempCartId back to client as a cookie
-                    context.Session[CartSessionKey] = tempCartId.ToString();
-                }
-                
-            }
-            return context.Session[CartSessionKey].ToString();
-        }
-        // When a user has logged in, migrate their shopping cart to
-        // be associated with their username
-       /* public void MigrateCart(string userName)
-        {
-            var shoppingCart = db.Carts.Where(
-                c => c.CartId == ShoppingCartId);
-
-            foreach (Cart item in shoppingCart)
-            {
-                item.CartId = userName;
-            }
-            db.SaveChanges();
-        }
-       */
+      
     }
 }
